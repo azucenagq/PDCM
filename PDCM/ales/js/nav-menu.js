e3c85a546bc7c1c23d4776eddca7b999c77d7c1c
dario <gil.q.c.dq@gmail.com>
2019-02-12 11:11:18 -0600

    pussh

--- PDCM/ales/js/nav-menu.js
+++ PDCM/ales/js/nav-menu.js
@@ -1,37 +1,65 @@
 (function($) {
 
-    'use strict';
+    "use strict";
 
-    $(document).ready(function() {
-
-        var nav = $(this);
-        nav.find('li ul').parent().addClass('has-sub');
-        nav.find(".has-sub").prepend('<span class="submenu-button fas"></span>');
-        $('.has-sub ul').hide();
-
-        // :: Submenu open and close click event
-        $('.has-sub > a,.submenu-button').on('click', function() {
-            $(this).each(function() {
-                if ($(this).siblings('ul').hasClass('open')) {
-                    $(this).siblings('ul').removeClass('open').slideUp().parent('li').removeClass('active');
+    $.fn.menumaker = function(options) {
+        var nav = $(this),
+            settings = $.extend({
+                format: "dropdown",
+                sticky: false
+            }, options);
+        return this.each(function() {
+            $(this).find(".navbar-toggler").on('click', function() {
+                $(this).toggleClass('menu-opened');
+                var mainmenu = $(this).next('ul');
+                if (mainmenu.hasClass('open')) {
+                    mainmenu.slideToggle().removeClass('open');
                 } else {
-                    $('.has-sub ul').removeClass('open').slideUp().parent('li').removeClass('active');
-                    $(this).siblings('ul').addClass('open').slideDown().parent('li').addClass('active');
+                    mainmenu.slideToggle().addClass('open');
+                    if (settings.format === "dropdown") {
+                        mainmenu.find('ul').show();
+                    }
                 }
             });
+            nav.find('.navbar-nav li ul').parent().addClass('has-sub');
+            var multiTg = function() {
+                nav.find(".has-sub").prepend('<span class="submenu-button"></span>');
+                nav.find('.submenu-button').on('click', function() {
+                    $(this).toggleClass('submenu-opened');
+
+                    if ($(this).siblings('ul').hasClass('open')) {
+                        $(this).siblings('ul').removeClass('open').slideToggle();
+                        $(this).parent('li').removeClass('active');
+                    } else {
+                        $(this).siblings('ul').addClass('open').slideToggle();
+                        $(this).parent('li').addClass('active');
+                    }
+                });
+            };
+            if (settings.format === 'multitoggle') multiTg();
+            else nav.addClass('dropdown');
+            if (settings.sticky === true) nav.css('position', 'fixed');
+            var resizeFix = function() {
+                var mediasize = 991;
+                if ($(window).width() > mediasize) {
+                    nav.find('ul').show();
+                }
+            };
+            resizeFix();
+            return $(window).on('resize', resizeFix);
         });
 
-        // :: Main menu open and close click event
-        $('.nav-control').on('click', function() {
-            if ($('body').hasClass('navbar-open')) {
-                $('body.navbar-open').removeClass('navbar-open');
-            } else {
-                $('body.navbar-open').removeClass('navbar-open');
-                $('body').addClass('navbar-open');
-            }
+    };
+
+    $(document).ready(function() {
+        $("nav").menumaker({
+            format: "multitoggle"
         });
 
-        // :: Menu selector and apply active and current class on menu
+        /*------------------------------------
+            Menu Selector
+        --------------------------------------*/
+
         var urlparam = window.location.pathname.split('/');
         var menuselctor = window.location.pathname;
         if (urlparam[urlparam.length - 1].length > 0)
@@ -39,7 +67,24 @@
         else
             menuselctor = urlparam[urlparam.length - 2];
 
-        $('nav .main-menu li').find('a[href="' + menuselctor + '"]').closest('li').addClass('active').parents().eq(1).addClass('current');
+        $('.navbar-nav li').find('a[href="' + menuselctor + '"]').closest('li').addClass('active').parents().eq(1).addClass('current');
+        $('.navbar-nav li.has-sub ul li').find('a[href="' + menuselctor + '"]').parents().eq(4).addClass('current');
+
     });
+
+        /*------------------------------------
+            Toggle Search
+        --------------------------------------*/
+
+        $(".navbar-default .attr-nav").each(function(){  
+            $("li.search > a", this).on("click", function(e){
+                e.preventDefault();
+                $(".top-search").slideToggle();
+            });
+        });
+
+        $(".input-group-addon.close-search").on("click", function(){
+            $(".top-search").slideUp();
+        });   
 
 })(jQuery);
